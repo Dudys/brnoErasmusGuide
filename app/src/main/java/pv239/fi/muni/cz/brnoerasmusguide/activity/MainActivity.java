1b0376cdbd9419c324ccd404929770cd4e6a61ca
Jakub Fišer <jakub.fiser@mautilus.com>
2016-05-19 16:40:20 +0200

    Navigation menu logic moved to MainActivity

--- app/src/main/java/pv239/fi/muni/cz/brnoerasmusguide/activity/MainActivity.java
+++ app/src/main/java/pv239/fi/muni/cz/brnoerasmusguide/activity/MainActivity.java
@@ -1,23 +1,69 @@
 package pv239.fi.muni.cz.brnoerasmusguide.activity;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 import pv239.fi.muni.cz.brnoerasmusguide.R;
-import pv239.fi.muni.cz.brnoerasmusguide.dataClasses.Building;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        ButterKnife.bind(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        Intent i;
+        if (id == R.id.nav_accommodation) {
+            i = new Intent(this, AccommodationListActivity.class);
+        } else if (id == R.id.nav_canteen) {
+            i = new Intent(this, BuildingListActivity.class);
+        } else if (id == R.id.nav_faculty) {
+            i = new Intent(this, BuildingListActivity.class);
+        } else if (id == R.id.nav_event) {
+            i = new Intent(this, EventsActivity.class);
+        }
+        else {
+            return false;
+        }
+        startActivity(i);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
